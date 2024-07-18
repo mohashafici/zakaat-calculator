2daf154c8c5aa26856af5192106bf5dd98ecef0e
mohashafici <mohashafici44@gmail.com>
2024-07-18 19:47:55 +0300

    Add Firebase configuration and Tailwind CSS setup

---
+++ src/firebase.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCypYlb4eMHDTdSDtewTHtrRsrXyvd-fZ8",
+  authDomain: "zakaat-calculator.firebaseapp.com",
+  projectId: "zakaat-calculator",
+  storageBucket: "zakaat-calculator.appspot.com",
+  messagingSenderId: "128085654866",
+  appId: "1:128085654866:web:181428a734323419db8e8b",
+  measurementId: "G-DNZH5D27PR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+export { auth };
